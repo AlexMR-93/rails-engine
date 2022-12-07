74a5323cea0aa05ef05b8fec57bdec533901db5e
Alex M <alexm0717@hotmail.com>
2022-12-06 23:39:27 -0800

    create and update item

--- spec/requests/merchant_request_spec.rb
+++ spec/requests/merchant_request_spec.rb
@@ -24,10 +24,10 @@ describe("Merchant API") do
     get("/api/v1/merchants/#{id}")
     merchant = JSON.parse(response.body,     symbolize_names: true)
     expect(response).to(be_successful)
-    expect(merchant).to(have_key(:id))
-    expect(merchant[:id]).to(eq(id))
-    expect(merchant).to(have_key(:name))
-    expect(merchant[:name]).to(be_a(String))
+    expect(merchant[:data]).to(have_key(:id))
+    expect(merchant[:data][:id]).to((be_a(String)))
+    expect(merchant[:data][:attributes]).to(have_key(:name))
+    expect(merchant[:data][:attributes][:name]).to(be_a(String))
   end
 
   it("get all items from a given merchant ID") do
@@ -53,5 +53,19 @@ describe("Merchant API") do
       expect(item[:attributes]).to(have_key(:merchant_id))
       expect(item[:attributes][:merchant_id]).to(be_a(Integer))
     end
+  end
+
+  it("can update an existing book") do
+    id = create(:item).id
+    previous_description = Item.last.description
+    item_params = {description: "Butter"}
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+  # We include this header to make sure that these params are passed as JSON rather than as plain text
+    patch("/api/v1/items/#{id}",     headers: headers,     params: JSON.generate({item: item_params}))
+    item = Item.find_by(    id: id)
+    expect(response).to(be_successful)
+    expect(item.description).to_not(eq(previous_description))
+    expect(item.description).to(eq("Butter"))
   end
 end
